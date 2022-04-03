9c8f32074ff4fcd9b2b81c6a573f2690f70e1b04
Andre Ferreira <andre.souza@sensedia.com>
2022-04-03 16:10:28 -0300

    Added repository for query order by id

--- src/main/java/br/dev/multicode/repositories/OrderRepository.java
+++ src/main/java/br/dev/multicode/repositories/OrderRepository.java
@@ -2,8 +2,11 @@ package br.dev.multicode.repositories;
 
 import br.dev.multicode.entities.Order;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
+import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
 
 @ApplicationScoped
 public class OrderRepository implements PanacheRepository<Order> {
@@ -13,5 +16,11 @@ public class OrderRepository implements PanacheRepository<Order> {
   {
     this.persistAndFlush(order);
     return order.getId();
+  }
+
+  public Order findOrderById(UUID orderId) {
+    return this.find("order_id=:orderId", Parameters.with("orderId", orderId.toString())).firstResultOptional()
+        .orElseThrow(() ->
+            new NotFoundException("Order not found by id=".concat(orderId.toString())));
   }
 }
