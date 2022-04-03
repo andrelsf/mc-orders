388319c2ad20cfc69ba64668de5cdcd960e09791
Andre Ferreira <andre.souza@sensedia.com>
2022-04-03 16:09:06 -0300

    Added resource GET order by id

--- src/main/java/br/dev/multicode/api/resources/OrderResource.java
+++ src/main/java/br/dev/multicode/api/resources/OrderResource.java
@@ -1,16 +1,21 @@
 package br.dev.multicode.api.resources;
 
 import br.dev.multicode.api.http.requests.OrderRequest;
+import br.dev.multicode.api.http.responses.OrderResponse;
 import br.dev.multicode.services.OrderService;
 import java.net.URI;
+import java.util.UUID;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/api/orders")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -26,6 +31,16 @@ public class OrderResource {
     final String orderId = orderService.create(orderRequest);
     return Response.created(URI.create("/api/orders/".concat(orderId)))
         .header("id", orderId)
+        .build();
+  }
+
+  @GET
+  @Path("/{orderId}")
+  public Response getOrder(@PathParam("orderId") UUID orderId)
+  {
+    final OrderResponse orderResponse = orderService.getOrderById(orderId);
+    return Response.status(Status.OK)
+        .entity(orderResponse)
         .build();
   }
 }
