e9e93edba47de6740f59b774d58cf3d9401c59a3
Andre Ferreira <andre.souza@sensedia.com>
2022-04-03 16:09:47 -0300

    Added service for GET Order by id

--- src/main/java/br/dev/multicode/services/OrderService.java
+++ src/main/java/br/dev/multicode/services/OrderService.java
@@ -1,9 +1,13 @@
 package br.dev.multicode.services;
 
 import br.dev.multicode.api.http.requests.OrderRequest;
+import br.dev.multicode.api.http.responses.OrderResponse;
+import java.util.UUID;
 
 public interface OrderService {
 
   String create(OrderRequest orderRequest);
+
+  OrderResponse getOrderById(UUID orderId);
 
 }
--- src/main/java/br/dev/multicode/services/impl/OrderServiceImpl.java
+++ src/main/java/br/dev/multicode/services/impl/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package br.dev.multicode.services.impl;
 
 import br.dev.multicode.api.http.requests.OrderRequest;
+import br.dev.multicode.api.http.responses.OrderResponse;
 import br.dev.multicode.entities.Order;
 import br.dev.multicode.repositories.OrderRepository;
 import br.dev.multicode.services.OrderService;
+import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -17,5 +19,11 @@ public class OrderServiceImpl implements OrderService {
   public String create(OrderRequest orderRequest)
   {
     return orderRepository.save(Order.of(orderRequest));
+  }
+
+  @Override
+  public OrderResponse getOrderById(UUID orderId) {
+    final Order order = orderRepository.findOrderById(orderId);
+    return order.toResponse();
   }
 }
