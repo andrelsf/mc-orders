5db4eb6d897b8d4ec1d12c207bf58e67558211a5
Andre Ferreira <andre.souza@sensedia.com>
2022-04-04 16:42:34 -0300

    Refactoring

--- src/main/java/br/dev/multicode/services/kafka/producers/OrderProducer.java
+++ src/main/java/br/dev/multicode/services/kafka/producers/OrderProducer.java
@@ -1,19 +1,32 @@
 package br.dev.multicode.services.kafka.producers;
 
 import br.dev.multicode.models.OrderMessage;
+import java.util.concurrent.CompletableFuture;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class OrderProducer {
+
+  private final Logger log = Logger.getLogger(this.getClass());
 
   @Inject @Channel("sec-new-order")
   Emitter<OrderMessage> emitter;
 
   public void sendOrderToKafka(OrderMessage orderMessage)
   {
-    emitter.send((orderMessage));
+    emitter.send(Message.of(orderMessage)
+        .withAck(() -> {
+            log.infof("Message sent successfully. eventId=%s", orderMessage.getEventId());
+            return CompletableFuture.completedFuture(null);
+        })
+        .withNack(throwable -> {
+          log.infof("Message sent failed. ERROR: %s", throwable.getMessage());
+          return CompletableFuture.completedFuture(null);
+        }));
   }
 }
