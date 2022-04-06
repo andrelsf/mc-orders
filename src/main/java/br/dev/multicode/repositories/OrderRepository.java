747c9d6c71dfab0d15bfc96cc2279f6b3d296d4c
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 20:46:00 -0300

    Fixed method save and added updateStatusById method

--- src/main/java/br/dev/multicode/repositories/OrderRepository.java
+++ src/main/java/br/dev/multicode/repositories/OrderRepository.java
@@ -1,6 +1,7 @@
 package br.dev.multicode.repositories;
 
 import br.dev.multicode.entities.Order;
+import br.dev.multicode.enums.OrderStatus;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
 import java.util.UUID;
@@ -11,16 +12,25 @@ import javax.ws.rs.NotFoundException;
 @ApplicationScoped
 public class OrderRepository implements PanacheRepository<Order> {
 
-  @Transactional // Event here for put in Kafka
-  public String save(final Order order)
+  @Transactional
+  public Order save(final Order order)
   {
     this.persistAndFlush(order);
-    return order.getId();
+    return order;
   }
 
   public Order findOrderById(UUID orderId) {
     return this.find("order_id=:orderId", Parameters.with("orderId", orderId.toString())).firstResultOptional()
         .orElseThrow(() ->
             new NotFoundException("Order not found by id=".concat(orderId.toString())));
+  }
+
+  @Transactional
+  public void updateStatusById(UUID orderId, OrderStatus newStatus)
+  {
+    Order order = this.findOrderById(orderId);
+    if (!order.getStatus().equals(newStatus)) {
+      order.setStatus(newStatus);
+    }
   }
 }
